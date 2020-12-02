b8bed2591a35905878e8c76a375e249897c06bb9
Voya <voya@voyacode.com>
2020-12-02 20:49:02 +0200

    Remove duplication from day 1 solution

--- day1.ts
+++ day1.ts
@@ -3,14 +3,13 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("inputs/day1.txt", "utf8");
 const numbers = input.split("\n").map(str => +str);
-const numbersSet = new Set(input.split("\n").map(str => +str));
+const numbersSet = new Set(numbers);
 
 function part1() {
   const sum = 2020;
   for (let number of numbersSet) {
     const requiredNumber = sum - number;
     if (numbersSet.has(requiredNumber)) {
-      // console.log(number, requiredNumber);
       return number * requiredNumber;
     }
   }
@@ -22,12 +21,10 @@ function part2() {
     for (let j = i + 1; j < numbers.length; j++) {
       const requiredNumber = sum - numbers[i] - numbers[j];
       if (numbersSet.has(requiredNumber)) {
-        // console.log(numbers[i], numbers[j], requiredNumber);
         return numbers[i] * numbers[j] * requiredNumber;
       }
     }
   }
-  // return masses.reduce((sum, mass) => sum + getTotalFuel(mass), 0);
 }
 
 console.log("Part 1", part1());
